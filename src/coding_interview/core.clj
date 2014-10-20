97d3b3e61cead62dfeca7f430faf40c4c2a3925e
David W. Body <davidbody@bigcreek.com>
2014-10-20 14:49:40 -0500

    Use mapcat instead of flatten

--- src/coding_interview/core.clj
+++ src/coding_interview/core.clj
@@ -4,6 +4,5 @@
   [s max-consecutive-chars]
   "Return another string that is similar to the input string, but with certain characters removed. It's going to remove characters from consecutive runs of the same character, where the length of the run is greater than the input parameter."
   (->> (partition-by char s)
-       (map (partial take max-consecutive-chars))
-       (flatten)
+       (mapcat #(take max-consecutive-chars %))
        (apply str)))
